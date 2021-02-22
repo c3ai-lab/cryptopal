33ee8e624f7c11dcaf504e68383763f4d0cf9754
LarsRa <lars.ra@web.de>
2021-02-22 11:15:23 +0100

    add database connection with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv/config');
 
 const app = express();
+
+// connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  },
+  () => {
+    console.log('connected to db');
+  }
+);
 
 // start server
 const port = 4000;
