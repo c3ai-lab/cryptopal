86035b1cb1fa4f8a470c225ee6950cf6aff4e27b
LarsRa <lars.ra@web.de>
2021-02-25 17:20:20 +0100

    add login route

--- backend/controllers/authorization.js
+++ backend/controllers/authorization.js
@@ -1,24 +1,26 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const { registerValidation } = require('../helper/validation');
+const { registerValidation, loginValidation } = require('../helper/validation');
 
+/** **********************REGISTER HANDLER*********************** */
 exports.register = async (req, res) => {
-  // validate data before creating a user
+  // validate received data before creating a user
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   // check if the user is already in the database
-  const emailExists = await User.findOne({ email: { value: req.body.email } });
+  const emailExists = await User.findOne({ loginName: req.body.email });
   if (emailExists) return res.status(400).send('Email already exists');
 
   // hash the password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-  // create user
+  // create user with received data
   const user = new User({
-    name: `${req.body.givenName} ${req.body.familyName}`,
+    loginName: req.body.email,
     givenName: req.body.givenName,
     familyName: req.body.familyName,
     email: [{ value: req.body.email, type: 'private', primary: true }],
@@ -41,4 +43,26 @@ exports.register = async (req, res) => {
   } catch (err) {
     res.status(400).send(err); // send db error
   }
+};
+
+/** **********************LOGIN HANDLER*********************** */
+exports.login = async (req, res) => {
+  // validate received data
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // check if the user is already in the database
+  const user = await User.findOne({ loginName: req.body.email });
+  if (!user) return res.status(400).send('Invalid email');
+
+  // check if password is correct
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) {
+    return res.status(400).send('Invalid password');
+  }
+
+  // create and assign a token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.setHeader('auth-token', token);
+  res.send(token);
 };
--- backend/routes/authorization.js
+++ backend/routes/authorization.js
@@ -3,6 +3,10 @@ const authorization = require('../controllers/authorization');
 
 const router = express.Router();
 
+// route to register a new user with authorization controller
 router.post('/register', authorization.register);
+
+// route to log in a user with authorization controller
+router.post('/login', authorization.login);
 
 module.exports = router;
