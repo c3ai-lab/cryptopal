e04b07e817580db8edeacef83282640335deefbc
LarsRa <lars.ra@web.de>
2021-02-25 12:09:33 +0100

    Defined user schema

---
+++ backend/models/User.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    min: 6,
+    max: 255,
+  },
+  givenName: {
+    type: String,
+    min: 6,
+    max: 255,
+  },
+  familyName: {
+    type: String,
+    min: 6,
+    max: 255,
+  },
+  email: [
+    {
+      value: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255,
+      },
+      primary: {
+        type: Boolean,
+        required: true,
+        min: 6,
+        max: 255,
+      },
+      type: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255,
+      },
+    },
+  ],
+  address: {
+    streetAddress: {
+      type: String,
+      min: 6,
+      max: 255,
+    },
+    locality: {
+      type: String,
+      min: 6,
+      max: 255,
+    },
+    region: {
+      type: String,
+      min: 6,
+      max: 255,
+    },
+    postalCode: {
+      type: String,
+      min: 1,
+      max: 255,
+    },
+    country: {
+      type: String,
+      min: 6,
+      max: 255,
+    },
+  },
+  verifiedAccount: {
+    type: Boolean,
+    required: true,
+  },
+  payerId: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 1024,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
