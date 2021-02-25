685906142121089fd96857c09d4f64302943b8dd
LarsRa <lars.ra@web.de>
2021-02-25 12:48:59 +0100

    add validation for authorization

---
+++ backend/helper/validation.js
@@ -0,0 +1,33 @@
+/*
+ * Validation of send authentification data before
+ * interacting with database.
+ */
+const Joi = require('@hapi/joi');
+
+const registerValidation = (data) => {
+  // set up joi validation of registration data
+  const schema = Joi.object({
+    givenName: Joi.string().required(),
+    familyName: Joi.string().required(),
+    password: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    streetAddress: Joi.string().required(),
+    locality: Joi.string().required(),
+    country: Joi.string().required(),
+  });
+
+  return schema.validate(data);
+};
+
+const loginValidation = (data) => {
+  // set up joi validation of login data
+  const schema = Joi.object({
+    email: Joi.string().min(6).required(),
+    password: Joi.string().min(6).required(),
+  });
+
+  return schema.validate(data);
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
