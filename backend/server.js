a664e26ea20c92865a2ca4964fa239116bb7b929
LarsRa <lars.ra@web.de>
2021-02-25 12:24:26 +0100

    add body parser middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv/config');
 
 const app = express();
+
+// add middleware
+app.use(bodyParser.json());
 
 // use routes
 app.use('/authorization', require('./routes/authorization'));
