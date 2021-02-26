f9a9b37702530935afcf2cb6bd04f0f9c1932879
LarsRa <lars.ra@web.de>
2021-02-26 14:25:02 +0100

    setup frontend with basic layout

---
+++ frontend/src/layouts/components/navbar/NavbarUser.js
@@ -0,0 +1,122 @@
+import React from "react"
+import {
+  UncontrolledDropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
+  Media,
+  Badge
+} from "reactstrap"
+import PerfectScrollbar from "react-perfect-scrollbar"
+import * as Icon from "react-feather"
+import { history } from "../../../history"
+
+
+const UserDropdown = props => {
+  return (
+    <DropdownMenu right>
+      <DropdownItem tag="a" href="#">
+        <Icon.User size={14} className="mr-50" />
+        <span className="align-middle">My Profile</span>
+      </DropdownItem>
+      <DropdownItem tag="a" href="#">
+        <Icon.Mail size={14} className="mr-50" />
+        <span className="align-middle">My Inbox</span>
+      </DropdownItem>
+      <DropdownItem divider />
+      <DropdownItem
+        tag="a"
+        href="#"
+        onClick={e => history.push("/pages/login")}
+      >
+        <Icon.Power size={14} className="mr-50" />
+        <span className="align-middle">Log Out</span>
+      </DropdownItem>
+    </DropdownMenu>
+  )
+}
+
+class NavbarUser extends React.PureComponent {
+
+  render() {
+    return (
+      <ul className="nav navbar-nav navbar-nav-user float-right">
+        <UncontrolledDropdown
+          tag="li"
+          className="dropdown-notification nav-item"
+        >
+          <DropdownToggle tag="a" className="nav-link nav-link-label">
+            <Icon.Bell size={21} />
+            <Badge pill color="primary" className="badge-up">
+              1
+            </Badge>
+          </DropdownToggle>
+          <DropdownMenu tag="ul" right className="dropdown-menu-media">
+            <li className="dropdown-menu-header">
+              <div className="dropdown-header mt-0">
+                <h3 className="text-white">1 New</h3>
+                <span className="notification-title">App Notifications</span>
+              </div>
+            </li>
+            <PerfectScrollbar
+              className="media-list overflow-hidden position-relative"
+              options={{
+                wheelPropagation: false
+              }}
+            >
+              <div className="d-flex justify-content-between">
+                <Media className="d-flex align-items-start">
+                  <Media left href="#">
+                    <Icon.File className="font-medium-5 warning" size={21} />
+                  </Media>
+                  <Media body>
+                    <Media heading className="warning media-heading" tag="h6">
+                      Confirm email address
+                    </Media>
+                    <p className="notification-text">
+                      To activate the account confirm your email address.
+                    </p>
+                  </Media>
+                  <small>
+                    <time
+                      className="media-meta"
+                      dateTime="2015-06-11T18:29:20+08:00"
+                    >
+                      Last month
+                    </time>
+                  </small>
+                </Media>
+              </div>
+            </PerfectScrollbar>
+            <li className="dropdown-menu-footer">
+              <DropdownItem tag="a" className="p-1 text-center">
+                <span className="align-middle">Read all notifications</span>
+              </DropdownItem>
+            </li>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+        <UncontrolledDropdown tag="li" className="dropdown-user nav-item">
+          <DropdownToggle tag="a" className="nav-link dropdown-user-link">
+            <div className="user-nav d-sm-flex d-none">
+              <span className="user-name text-bold-600">
+                {this.props.userName}
+              </span>
+              <span className="user-status">Verified</span>
+            </div>
+            <span data-tour="user">
+              <img
+                src={this.props.userImg}
+                className="round"
+                height="40"
+                width="40"
+                alt="avatar"
+              />
+            </span>
+          </DropdownToggle>
+          <UserDropdown {...this.props} />
+        </UncontrolledDropdown>
+      </ul>
+    )
+  }
+}
+export default NavbarUser
