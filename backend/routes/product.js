6d4e7b884d982beee777fe587639e143152b4718
LarsRa <lars.ra@web.de>
2021-03-12 10:20:21 +0100

    add validation to products routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const product = require('../controllers/product');
+const verifyToken = require('../middleware/verifyToken');
 
 const router = express.Router();
 
 // route to add new product with product controller
-router.post('/', product.addProduct);
+router.post('/', verifyToken, product.addProduct);
 
 module.exports = router;
