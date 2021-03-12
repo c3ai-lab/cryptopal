777a66d6abc5fde7f1df2c9d99326e2c698d5777
LarsRa <lars.ra@web.de>
2021-03-12 10:20:46 +0100

    add foreign key for merchant to products

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
+  merchant_id: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
