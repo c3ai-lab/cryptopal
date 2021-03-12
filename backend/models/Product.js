0f1950b0cecdc09b268df517c26bcdd0686ed996
LarsRa <lars.ra@web.de>
2021-03-12 16:00:57 +0100

    adjust model to pp restrictions

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    min: 6,
+    max: 50,
+  },
   merchant_id: {
     type: String,
     required: true,
@@ -8,36 +13,44 @@ const productSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    max: 255,
+    min: 1,
+    max: 127,
   },
   description: {
     type: String,
-    max: 1024,
+    min: 1,
+    max: 256,
   },
   type: {
     type: String,
     required: true,
-    max: 255,
+    min: 1,
+    max: 24,
   },
   category: {
     type: String,
-    max: 255,
+    min: 4,
+    max: 256,
   },
   img_url: {
     type: String,
-    max: 255,
+    min: 1,
+    max: 2000,
   },
   home_url: {
     type: String,
-    max: 255,
+    min: 1,
+    max: 2000,
   },
   create_time: {
     type: String,
-    max: 255,
+    min: 20,
+    max: 64,
   },
   update_time: {
     type: String,
-    max: 255,
+    min: 20,
+    max: 64,
   },
   links: [
     {
