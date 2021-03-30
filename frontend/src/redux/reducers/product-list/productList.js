7ce766a914c050bc4c57f5dbce3fef4044d40a7d
LarsRa <lars.ra@web.de>
2021-03-30 17:15:21 +0200

    add products reducer to redux

---
+++ frontend/src/redux/reducers/product-list/productList.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-case-declarations */
+const initialState = {
+  data: [],
+  totalPages: 0,
+  totalItems: 0,
+  params: {
+    page_size: 5,
+    page: 1,
+    total_required: true
+  }
+};
+
+const DataListReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'GET_DATA':
+      return {
+        ...state,
+        data: action.data,
+        totalPages: action.totalPages,
+        totalItems: action.totalItems,
+        params: action.params
+      };
+    case 'ADD_DATA':
+      state.data.push({ ...action.obj });
+
+      return {
+        ...state,
+        data: state.data,
+        totalItems: state.totalItems + 1
+      };
+    case 'UPDATE_DATA':
+      state.data.find((item) => {
+        if (item._id === action.obj._id) {
+          return Object.assign(item, { ...action.obj });
+        } else {
+          return item;
+        }
+      });
+      return { ...state };
+    case 'DELETE_DATA':
+      let index = state.data.findIndex((item) => item._id === action.obj._id);
+      let updatedData = [...state.data];
+      updatedData.splice(index, 1);
+      return {
+        ...state,
+        data: updatedData,
+        totalItems: state.totalItems - 1
+      };
+    default:
+      return state;
+  }
+};
+
+export default DataListReducer;
--- frontend/src/redux/reducers/rootReducer.js
+++ frontend/src/redux/reducers/rootReducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import customizer from './customizer/';
+import productListReducer from './product-list/productList';
 import authReducer from './auth/authReducer';
 import errorReducer from './errors/errorReducer';
 
 const rootReducer = combineReducers({
   customizer: customizer,
   auth: authReducer,
-  error: errorReducer
+  error: errorReducer,
+  productList: productListReducer
 });
 
 export default rootReducer;
