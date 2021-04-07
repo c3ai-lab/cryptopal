736308f21fce403437c69fd0b92e595eaa4e8942
LarsRa <lars.ra@web.de>
2021-04-07 21:07:06 +0200

    delete .docz folder

--- docs/.docz/doczrc.js
+++
@@ -1,8 +0,0 @@
-export default {
-    title: "CryptoPal Developer Docs",
-    description: "Official docs of the CryptoPal api.",
-    themeConfig: {
-      initialColorMode: 'dark',
-    },
-    
-  }
--- docs/.docz/src/gatsby-theme-docz/theme/styles.js
+++
@@ -1,5 +0,0 @@
-import styles from 'gatsby-theme-docz/src/theme/styles';
-
-styles.root.fontSize = 2 // or whatever you'd like
-
-export default styles
