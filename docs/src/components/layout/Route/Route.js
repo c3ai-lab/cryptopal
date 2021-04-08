2e4b075afe8586c92a048809cc75595fecc24998
LarsRa <lars.ra@web.de>
2021-04-08 12:04:41 +0200

    change route color for different operations

--- docs/src/components/layout/Route/Route.js
+++ docs/src/components/layout/Route/Route.js
@@ -4,9 +4,26 @@ import "./Route.css"
 class Route extends React.Component {
 
     render () {
+        let badgeColor;
+        switch(this.props.method){
+            case "GET":
+                badgeColor = "route-badge-get"
+                break;
+            case "POST":
+                badgeColor = "route-badge-post"
+                break;
+            case "PATCH":
+                badgeColor = "route-badge-patch"
+                break;
+            case "DELETE":
+                badgeColor = "route-badge-delete"
+                break;
+            default:
+                break;
+        }
         return( 
         <div className="route-wrapper">
-            <div className="route-badge">{this.props.method}</div>
+            <div className={"route-badge " + badgeColor}>{this.props.method}</div>
             <span className="route-path">{this.props.path}</span>
         </div>
         );
